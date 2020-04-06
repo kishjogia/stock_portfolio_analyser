180ccd6b77ac05950487ad84bc77d4937b3bc396
Kish Jogia <kishjogia@yahoo.co.uk>
2020-04-06 12:05:39 +0100

    Change the stock list file to add quantity of stocks owned and original price paid

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # stock_portfolio_analyser
+
+### Usage of stock_list.txt
+
+Add the details of the stock that you own
+A new line is required for each stock entry
+```
+ticker_name,quanitity,price_paid
+```
--- stock_portfolio_analyser.py
+++ stock_portfolio_analyser.py
@@ -6,20 +6,34 @@
 # [ ] 5. Show graphical each stock
 # [ ] 6. Store stock list in a DB, be able to add/remove via UI
 
+import os
 from yahoo_fin.stock_info import get_live_price
 
-stock_file = "/storage/emulated/0/development/stock_portfolio_analyser/stock_list.txt"
+test = os.uname()
+if test[0] == "Linux":
+	stock_file = "/home/kishj/OneDrive/development/stock_portfolio_analyser/stock_list.txt"
+else:
+	stock_file = "/storage/emulated/0/development/stock_portfolio_analyser/stock_list.txt"
 
 def get_stock_list():
+	rows, cols = (100,3)
+	item = [[0]*cols]*rows
+
 	# Read the file
 	file1 = open(stock_file, "r")
-	read_data = file1.read()
+	read_data = file1.readlines()
 	file1.close
-	
+
+	i = 0
 	# Parse the file, getting each ticker name
-	list = read_data.split(",")
-	
-	return(list)
+	for line in read_data:
+		item[i] = read_data[i].split(",")
+		if item[i][2].endswith("\n"):
+			item[i][2] = item[i][2][:-1]				# remove the trailing \n
+
+		i = i + 1
+
+	return(item)
 
 def get_current_price(stock):
 	data = get_live_price(stock)
@@ -36,6 +50,14 @@ if __name__ == "__main__":
 
 	# Get the latest price of each of the shares	
 	for stock in stk_list:
-		price = get_current_price(stock)
-		
-		print (stock, price)
+		if stock[0] != 0:
+			price = get_current_price(stock[0])
+
+			curr_value = (float(stock[1]) * price) / 100
+			
+			# Show the output
+			print (stock[0], \
+				"Curr Price: {:.2f}p".format(price), \
+					"Curr Val: £ {:.2f}".format(curr_value), \
+						"Quantity: {:.2f}".format(float(stock[1])), \
+							"Cost/Share: {:.2f}p".format(float(stock[2])))
