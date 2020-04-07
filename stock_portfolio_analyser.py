231c65c03e31d66cac30311b5da78df3fe2fbb33
Kish Jogia <kishjogia@yahoo.co.uk>
2020-04-07 15:49:48 +0100

    Update the current stock data every 60 seconds - running in a thread

--- stock_portfolio_analyser.py
+++ stock_portfolio_analyser.py
@@ -1,12 +1,17 @@
 # ToDo:
 # [x] 1. Get stock price from web for a stock
-# [ ] 2. Auto update at configurable setting
-# [x] 3. View several stock prices, read from file
-# [ ] 4. Show different detail in a table
-# [ ] 5. Show graphical each stock
-# [ ] 6. Store stock list in a DB, be able to add/remove via UI
+# [x] 2. View several stock prices, read from file
+# [x] 3. Show different detail in a table
+# [x] 4. Auto update every 60 seconds (run in a thread)
+# [ ] 5. Get historical information for stock
+# [ ] 6. Put data into SQL DB
+# [ ] 7. Show graphical each stock
+# [ ] 8. Store stock list in a DB, be able to add/remove via UI
 
 import os
+import pandas as pd
+import time
+from threading import Thread
 from yahoo_fin.stock_info import get_live_price
 
 test = os.uname()
@@ -27,7 +32,7 @@ def get_stock_list():
 	i = 0
 	# Parse the file, getting each ticker name
 	for line in read_data:
-		item[i] = read_data[i].split(",")
+		item[i] = line.split(",")
 		if item[i][2].endswith("\n"):
 			item[i][2] = item[i][2][:-1]				# remove the trailing \n
 
@@ -35,29 +40,56 @@ def get_stock_list():
 
 	return(item)
 
-def get_current_price(stock):
-	data = get_live_price(stock)
+def get_current_data(stock):
+	global curr_value
+	global total_chnge
+	global percent_chng
+
+	data = get_live_price(stock[0])
+
+	curr_value = (float(stock[1]) * data) / 100
+	total_chnge = curr_value - ((float(stock[2])* float(stock[1])) / 100)
+	percent_chng = total_chnge / ((float(stock[2])* float(stock[1])) / 100) * 100
 	
 	return(data)
+
+def get_historical_data(stock):
+
+	return	
+
+def timer_60_secs(stock_list):
+	while True:
+		i = 0
+		# Get the latest price of each of the shares
+		for stock in stock_list:
+			if stock[0] != 0:
+				price = get_current_data(stock)
+				# Add the data into a new rows
+				df.loc[i] = [stock[0],price,curr_value,stock[1],stock[2],total_chnge,percent_chng]
+				
+				i = i + 1
+		print(df)
+		time.sleep(60)
 
 #********
 # Main
 #********
 
 if __name__ == "__main__":
+
 	# Read the list of shares from a CSV file
 	stk_list = get_stock_list()
 
-	# Get the latest price of each of the shares	
-	for stock in stk_list:
-		if stock[0] != 0:
-			price = get_current_price(stock[0])
+	# Add column into Pandas
+	df = pd.DataFrame(columns=["Ticker", "Price", "Value", "Total", "Cost/Share","Change", "% Change"])
 
-			curr_value = (float(stock[1]) * price) / 100
-			
+	# Get the data every minute
+	get_data_thread = Thread(target=timer_60_secs, args=(stk_list,))
+	get_data_thread.start()
+
+#	print(df)
+
+			#print ("Ticker |  Price   |  Value   |  Total   | Cst/Shre | Change  | % Chnge")
 			# Show the output
-			print (stock[0], \
-				"Curr Price: {:.2f}p".format(price), \
-					"Curr Val: £ {:.2f}".format(curr_value), \
-						"Quantity: {:.2f}".format(float(stock[1])), \
-							"Cost/Share: {:.2f}p".format(float(stock[2])))
+#			print("{:<6s} | {:>7.2f}p | £{:>7.2f} | {:>8.2f} | {:>7.2f}p | {:>7.2f} | {:>7.2f}%" \
+#				.format(stock[0],price,curr_value,float(stock[1]),float(stock[2]),total_chnge,percent_chng))
